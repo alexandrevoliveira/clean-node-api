f150c690a533dd6eac3706dd71893abb9e524a61
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-07-15 10:41:39 -0300

    refactor: organize project files

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,2 +1,2 @@
-export * from '@/domain/usecases/add-survey'
+export * from '@/domain/usecases/survey/add-survey'
 export * from '@/presentation/protocols'
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller-protocols.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller-protocols.ts
@@ -1,3 +1,3 @@
 export * from '@/domain/models/survey'
-export * from '@/domain/usecases/load-surveys'
+export * from '@/domain/usecases/survey/load-surveys'
 export * from '@/presentation/protocols'
