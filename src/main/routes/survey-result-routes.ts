c71f355059d5f0f55f27c0f6e8b2062e1c25dbcd
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-07-16 22:03:24 -0300

    feat: ensure save survey result route returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,8 @@
+import { adaptRoute } from '@/main/adapters/express-route-adapter'
+import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { auth } from '@/main/middlewares/auth'
+import { Router } from 'express'
+
+export default (router: Router): void => {
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+}
