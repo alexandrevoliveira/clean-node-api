d1be8a2488189066626004dd5e701b4e69445eaa
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-07-21 17:30:55 -0300

    docs: add signup api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
 import { badRequest, forbidden, notFound, serverError, unauthorized } from './components'
-import { loginPath, surveyPath } from './paths'
-import { accountSchema, apiKeyAuthSchema, errorSchema, loginParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema } from './schemas'
+import { loginPath, signUpPath, surveyPath } from './paths'
+import { accountSchema, apiKeyAuthSchema, errorSchema, loginParamsSchema, signUpParamsSchema, surveyAnswerSchema, surveySchema, surveysSchema } from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -23,12 +23,14 @@ export default {
   }],
   paths: {
     '/login': loginPath,
+    '/signup': signUpPath,
     '/surveys': surveyPath
   },
   schemas: {
     account: accountSchema,
     error: errorSchema,
     loginParams: loginParamsSchema,
+    signUpParams: signUpParamsSchema,
     surveyAnswer: surveyAnswerSchema,
     survey: surveySchema,
     surveys: surveysSchema
