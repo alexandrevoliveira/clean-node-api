5b21d6197888b9a5ee97615a1acdb08f07f7e93b
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-07-22 10:33:36 -0300

    docs: update api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -5,21 +5,33 @@ import schemas from './schemas'
 export default {
   openapi: '3.0.0',
   info: {
-    title: 'clean-node-api',
-    description: 'API do curso do Mango para realizar enquetes entre programadores',
+    title: 'Clean Node API',
+    description: 'Essa é a documentação da API feita pelo instrutor Rodrigo Manguinho no curso da Udemy de NodeJs usando Typescript, TDD, Clean Architecture e seguindo os princípios do SOLID e Design Patterns.',
     version: '1.0.0'
+  },
+  contact: {
+    name: 'Alexandre Oliveira',
+    email: 'alexandrevoliveira30@gmail.com',
+    url: 'https://www.linkedin.com/in/alexandrevoliveira'
   },
   license: {
     name: 'GPL-3.0-or-later',
     url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
+  externalDocs: {
+    description: 'Link para o treinamento completo',
+    url: 'https://www.udemy.com/course/tdd-com-mango/?referralCode=B53CE5CA2B9AFA5A6FA1'
+  },
   servers: [{
-    url: '/api'
+    url: '/api',
+    description: 'Servidor Principal'
   }],
   tags: [{
-    name: 'Login'
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
   }, {
-    name: 'Enquete'
+    name: 'Enquete',
+    description: 'APIs relacionadas a Enquete'
   }],
   paths,
   schemas,
