be3ddf511f0489ec8f5ad728b03c55572f1071f8
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-21 20:58:55 -0300

    refactor: change how ci displays logs via container build

--- test-ci.sh
+++ test-ci.sh
@@ -2,5 +2,5 @@
 
 set -o errexit
 
-docker build -t alexandrevoliveira/clean-node-api:latest -f ./Dockerfile.dev .
+docker build --no-cache --progress=plain -t alexandrevoliveira/clean-node-api:latest -f ./Dockerfile.dev .
 docker run -e CI=true alexandrevoliveira/clean-node-api npm run test:ci
